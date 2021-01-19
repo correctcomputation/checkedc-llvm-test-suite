1748c2f7b0ea1ee5c1273c10d2e6f8fbfd851ce9
Kyle Headley <kyle@correctcomputation.com>
2021-01-19 12:45:34 -0500

    manual - setup anagram

--- MultiSource/Benchmarks/Ptrdist/anagram/anagram.c
+++ MultiSource/Benchmarks/Ptrdist/anagram/anagram.c
@@ -481,10 +481,7 @@ void DumpWord(Quad * pq) {
 )                                       /* End of debug code */
 
 void DumpWords(void) {
-static int X;
   int i;
-  X = (X+1) & 1023;
-  if (X != 0) return;
     for (i = 0; i < cpwLast; i++) wprint(apwSol[i]->pchWord);
     printf("\n");
 }
